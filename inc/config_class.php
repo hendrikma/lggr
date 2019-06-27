b07003ed883d15b5c2b78c5cfb322825c5a8f80d
Unknown <xyz258@abwesend.de>
2019-06-27 21:05:24 +0200

    Change to local libs
    https://www.bachmann-lan.de/zentraler-logserver-mit-syslog-ng-mariadb-und-lggr-io-webinterface/

--- inc/config_class.php
+++ inc/config_class.php
@@ -11,7 +11,7 @@ class Config extends AbstractConfig {
         // Set your preferred language en_US, de_DE, or pt_BR
         $this->setLocale('en_US');
         
-        /* remote storage */
+        /* remote storage 
         $this->setUrlBootstrap('//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/');
         $this->setUrlJquery('//code.jquery.com/');
         $this->setUrlJqueryui('//code.jquery.com/ui/1.11.4/');
@@ -19,15 +19,15 @@ class Config extends AbstractConfig {
             '//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/');
         $this->setUrlChartjs('//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/');
         $this->setUrlJQCloud('//cdnjs.cloudflare.com/ajax/libs/jqcloud/1.0.4/');
+        */
+         local storage 
         
-        /* local storage */
-        /*
-         * $this->setUrlBootstrap('/contrib/bootstrap/');
-         * $this->setUrlJquery('/contrib/jquery/');
-         * $this->setUrlJqueryui('/contrib/jqueryui/');
-         * $this->setUrlJAtimepicker('/contrib/timepicker/');
-         * $this->setUrlChartjs('/contrib/chartjs/');
-         * $this->setUrlJQCloud('/contrib/jqcloud/');
-         */
+        $this->setUrlBootstrap('/logger/contrib/bootstrap/');
+        $this->setUrlJquery('/logger/contrib/jquery/');
+        $this->setUrlJqueryui('/logger/contrib/jqueryui/');
+        $this->setUrlJAtimepicker('/logger/contrib/timepicker/');
+        $this->setUrlChartjs('/logger/contrib/chartjs/');
+        $this->setUrlJQCloud('/logger/contrib/jqcloud/');
+        
     } // constructor
 } // class
